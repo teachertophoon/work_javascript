fa7a03ee4695a748dbec157071632f2a7f46816b
exemplaryer <exemplaryer@gmail.com>
2018-03-30 17:48:07 +0900

    자바스크립트 286쪽까지

---
+++ JavaScript01Basic/WebContent/myscript.js
@@ -0,0 +1,9 @@
+document.write("Hello World!");
+
+// id가 heading1인 헤딩요소를 찾아서 내용을 바꾼다.
+document.getElementById("heading1").innerHTML = "My Homepage";
+
+/*
+ 	이 코드는 웹 페이지의 헤딩의 내용을 변경한다.
+ */
+document.getElementById("heading2").innerHTML = "Your Homepage";
